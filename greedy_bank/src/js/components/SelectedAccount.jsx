4af87b9c6addc6b2622a21ab05e37ddca09c5b44
Sinethemba <sinethemba@wealthmigrate.com>
2018-10-24 21:40:36 +0000

    created handling functions for input and my two buttons

--- greedy_bank/src/js/components/SelectedAccount.jsx
+++ greedy_bank/src/js/components/SelectedAccount.jsx
@@ -2,6 +2,40 @@ import React from 'react';
 
 
 class SelectedAccount extends React.Component{ 
+    constructor(props){
+        super(props)
+        this.state = {ammount: 0}
+    }
+
+    // componentDidMount = () => {
+    //     const ammount = {"ammount": }
+    //     fetch('http://localhost:5000/accounts', {
+    //         method: 'post',
+    //         body: JSON.stringify(ammount)
+    //       })
+    //         .then((response) => response.json())
+    //         .then((responseJson) => {
+    //             setTimeout(() => this.props.fetchAccounts(responseJson.data), 2000);
+    //         })
+    // }
+
+    handleWithdraw = (event) => {
+        const ammount = {"ammount": -this.state.ammount}
+        console.log(ammount);
+        // event.preventDefault();
+        return ammount;
+    }
+
+    handleDeposit = (event) => {
+        const ammount = {"ammount": this.state.ammount}; 
+        console.log(ammount);
+        // event.preventDefault();
+        return ammount;
+    }
+
+    handleChange = (event) => {
+        this.setState({ammount: event.target.value}); 
+    }
 
     render(){ 
         return (
@@ -10,11 +44,11 @@ class SelectedAccount extends React.Component{
                 <br/>
                 <hr />
                  <h4>Selected Account: {this.props.account.accountNumber}</h4> 
-                <form action="" className='form-field'>
-                    <input type="text" placeholder='ammount' name='ammount'/>
-                    <button className='updateBalaceButton' >Withdraw</button> 
-                   <button className='updateBalaceButton'>Deposit</button>
-                </form>
+                <div className='form-field'>
+                    <input type="text" placeholder='ammount' name='ammount' value={this.state.value} onChange={this.handleChange} />
+                </div>
+                <button className='updateBalaceButton' onClick={this.handleWithdraw} >Withdraw</button> 
+                <button className='updateBalaceButton' onClick={this.handleDeposit}>Deposit</button>
             
             </div>
 
